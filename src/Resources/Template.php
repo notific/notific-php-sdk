3ac646b3f28ca5c694c87c6f77d8db577ef48c83
palokankare <kalle@klopal.com>
2018-04-10 23:54:24 +0300

    New template methods to send notifications more efficiently.

--- src/Resources/Template.php
+++ src/Resources/Template.php
@@ -50,14 +50,21 @@ class Template extends ApiResource
     public $updatedAt;
 
     /**
+     * @var array
+     */
+    private $data;
+
+    /**
      * Template constructor.
      *
      * @param array $attributes
-     * @param null  $notific
+     * @param null $notific
      */
     public function __construct(array $attributes, $notific = null)
     {
         parent::__construct($attributes, $notific);
+
+        $this->data = [];
     }
 
     /**
@@ -65,10 +72,59 @@ class Template extends ApiResource
      *
      * @return mixed
      */
-    public function sendTo($recipients)
+    public function sendTo(...$recipients)
     {
-        $data['recipients'] = is_array($recipients) ? $recipients : [$recipients];
+        $this->data['recipients'] = $this->flatten($recipients);
 
-        return $this->notific->sendPrivateNotification($this->id, $data);
+        return $this->notific->sendPrivateNotification($this->id, $this->data);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function send()
+    {
+        return $this->notific->sendPrivateNotification($this->id, $this->data);
+    }
+
+    /**
+     * @param mixed ...$recipients
+     * @return mixed
+     */
+    public function recipients(...$recipients)
+    {
+        $this->data['recipients'] = $this->flatten($recipients);
+
+        return $this->notific->sendPrivateNotification($this->id, $this->data);
+    }
+
+    /**
+     * @param mixed ...$tags
+     * @return $this
+     */
+    public function tags(...$tags)
+    {
+        $this->data['tags'] = $this->flatten($tags);
+
+        return $this;
+    }
+
+    /**
+     * @param mixed ...$channels
+     * @return $this
+     */
+    public function channels(...$channels)
+    {
+        $this->data['channels'] = $this->flatten($channels);
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function test()
+    {
+        return $this->notific->sendPrivateNotificationTest($this->id);
     }
 }
