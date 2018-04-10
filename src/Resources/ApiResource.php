e4d1364172f8954fa086f6da5d8aee7dae6f3305
palokankare <kalle@klopal.com>
2018-04-10 23:58:48 +0300

    Flatten method for splat arguments.

--- src/Resources/ApiResource.php
+++ src/Resources/ApiResource.php
@@ -19,7 +19,7 @@ class ApiResource
     /**
      * Create a new resource instance.
      *
-     * @param array   $attributes
+     * @param array $attributes
      * @param Notific $notific
      *
      * @return void
@@ -33,7 +33,7 @@ class ApiResource
 
     private function fill()
     {
-        foreach ($this->attributes as $key => $value) {
+        foreach($this->attributes as $key => $value) {
             $key = $this->camelCase($key);
 
             $this->{$key} = $value;
@@ -48,12 +48,37 @@ class ApiResource
     private function camelCase($key)
     {
         $parts = explode('_', $key);
-        foreach ($parts as $i => $part) {
+
+        foreach($parts as $i => $part) {
             if ($i !== 0) {
                 $parts[$i] = ucfirst($part);
             }
         }
 
         return str_replace(' ', '', implode(' ', $parts));
+    }
+
+    /**
+     * Flatten (multidimensional) arrays.
+     *
+     * @param $array
+     * @param $depth
+     * @return array
+     */
+    protected function flatten($array, $depth = INF)
+    {
+        $result = [];
+
+        foreach($array as $item) {
+            if (!is_array($item)) {
+                $result[] = $item;
+            } elseif ($depth === 1) {
+                $result = array_merge($result, array_values($item));
+            } else {
+                $result = array_merge($result, $this->flatten($item, $depth - 1));
+            }
+        }
+
+        return $result;
     }
 }
